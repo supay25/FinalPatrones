330e56f40852a54d59bd913bdd253e7c6749f7c1
Arturo <134749575+supay25@users.noreply.github.com>
2023-07-31 15:41:45 -0600

    Update FinalPatrones.java

--- FinalPatrones/src/finalpatrones/FinalPatrones.java
+++ FinalPatrones/src/finalpatrones/FinalPatrones.java
@@ -14,7 +14,7 @@ public class FinalPatrones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Sup");
+        System.out.println("Sussp");
     }
     
 }
