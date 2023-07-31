67e0ed640c7fff57986256eaf338d1acebafc9cf
supay25 <134749575+supay25@users.noreply.github.com>
2023-07-31 15:41:13 -0600

    A

--- FinalPatrones/src/finalpatrones/FinalPatrones.java
+++ FinalPatrones/src/finalpatrones/FinalPatrones.java
@@ -14,7 +14,7 @@ public class FinalPatrones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Supay2");
+        System.out.println("Sup");
     }
     
 }
