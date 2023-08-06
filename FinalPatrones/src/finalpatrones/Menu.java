6e2ce91a37087c18a0122201aaa67332288de140
supay25 <134749575+supay25@users.noreply.github.com>
2023-08-05 21:52:20 -0600

    Conexion establecida

--- FinalPatrones/src/finalpatrones/Menu.java
+++ FinalPatrones/src/finalpatrones/Menu.java
@@ -17,8 +17,8 @@ public class Menu extends javax.swing.JFrame {
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setTitle("Inventarios breska");
-       // rsscalelabel.RSScaleLabel.setScaleLabel(jl, "src/Imagenes/MenuFondo.png");
-     
+        Conexion con = new Conexion();
+        con.getConnection();
     }
 
 
